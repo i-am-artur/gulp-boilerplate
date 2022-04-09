852f1bd065a53c7a4982335ecb3ebe21b9d3033f
Artur <adrenalin247@gmail.com>
2022-04-09 13:50:16 +0300

    gulp boilerplate

---
+++ gulp/tasks/js.js
@@ -0,0 +1,42 @@
+// import webpack from 'webpack-stream';
+
+import eslint from 'gulp-eslint';
+import babel from 'gulp-babel';
+
+export const js = () => {
+  return app.gulp.src(app.path.src.js, { sourcemaps: true })
+    .pipe(app.plugins.plumber(
+      app.plugins.notify.onError({
+        title: 'JS',
+        message: 'Error: <%= error.message %>'
+      })
+    ))
+
+    .pipe(eslint({}))
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError())
+
+    // .pipe(babel({
+    //   presets: ['@babel/env']
+    // }))
+
+    // .pipe(app.gulp.dest(app.path.build.js))
+
+    // use webpack
+    // .pipe(webpack({
+    //   mode: 'development',
+    //   optimization: {
+    //     minimize: false,
+    //   },
+    //   output: {
+    //     filename: 'app.min.js'
+    //   }
+    // }))
+
+    .pipe(app.gulp.dest(app.path.build.js))
+    .pipe(app.plugins.browsersync.stream());
+};
